3953e74ed255e827a5956ae624f7f156485e44b5
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-23 22:40:06 +0600

    Добавлены модели Product, Category

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,3 +1,23 @@
+from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(null=False, blank=True, max_length=50)
+    desc = RichTextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50)
+    desc = RichTextField(null=False, blank=False)
+    category = models.ForeignKey(to='Category', on_delete=models.CASCADE, blank=False, null=False, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
+    image = models.ImageField(null=True, blank=True, upload_to='products/images/')
+
+    def __str__(self):
+        return self.name
+
