a6e9dde533a0b76863522a351eb496df86f58635
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-24 22:10:48 +0600

    Добавлены label для форм добавления товаров и категорий

--- src/shop/forms.py
+++ src/shop/forms.py
@@ -1,24 +1,36 @@
 from dataclasses import fields
 from django.forms import ModelForm
 from .models import Product, Category
+from django import forms
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'desc', 'category', 'price', 'image']
+        labels = {
+            'name':'Название товара',
+            'desc':'Описание товара',
+            'category':'Категория',
+            'price':'Цена',
+            'image':'Изображение',
+        }
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control name-input'}),
+            'category':forms.Select(attrs={'class':'form-control category-input'}),
+            'price':forms.TextInput(attrs={'class':'form-control price-input'}),
+            'image':forms.FileInput(attrs={'class':'form-control image-input'}),
+        }
     
 
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = ['name', 'desc']
+        labels = {
+            'name':'Название товара',
+            'desc':'Описание товара',
+        }
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control name-input'}),
+        }
 
-
-
-# class Product(models.Model):
-#     name = models.CharField(null=False, blank=False, max_length=50)
-#     desc = RichTextField(null=False, blank=False)
-#     category = models.ForeignKey(to='Category', on_delete=models.CASCADE, blank=False, null=False, related_name='products')
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     price = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
-#     image = models.ImageField(null=True, blank=True, upload_to='products/images/')
